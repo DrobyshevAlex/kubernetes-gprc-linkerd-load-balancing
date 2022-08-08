92f9d6afed872443b679162d3a7d76d70fe932f7
Drobyshev Alexander <drobyshev83@gmail.com>
2022-08-08 11:40:32 +0300

    gin added

--- auth/cmd/main.go
+++ auth/cmd/main.go
@@ -4,18 +4,19 @@ import (
 	"context"
 	"flag"
 	"log"
-	"strconv"
 	"time"
+	"os"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
+	"github.com/gin-gonic/gin"
 
 	"auth/models"
 )
 
 var (
-	addr = flag.String("addr", "app-user:50051", "the address to connect to")
-	//addr = flag.String("addr", "localhost:50051", "the address to connect to")
+	//addr = flag.String("addr", "app-user:50051", "the address to connect to")
+	addr = flag.String("addr", "localhost:50051", "the address to connect to")
 )
 
 func main() {
@@ -26,18 +27,27 @@ func main() {
 		log.Fatalf("did not connect: %v", err)
 	}
 	defer conn.Close()
-	c := models.NewAuthServiceClient(conn)
+	connAuth := models.NewAuthServiceClient(conn)
 
-	for i := 0; ; i++ {
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
 		ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 		defer cancel()
-
-		r, err := c.GetUser(ctx, &models.GetUserRequest{Username: "User " + strconv.Itoa(i)})
+		r, err := connAuth.GetUser(ctx, &models.GetUserRequest{Username: "User"})
 		if err != nil {
 			log.Printf("not exec: %v", err)
+			c.AbortWithStatusJSON(503, gin.H{"error": err})
 		} else {
 			log.Printf("Success: %s", r.Password)
+			c.JSON(200, gin.H{
+				"message": r.Password,
+			})
 		}
-		time.Sleep(time.Second * 1)
+	})
+
+	addr := os.Getenv("AUTH_ADDR")
+	if len(addr) == 0 {
+		addr = "0.0.0.0:8080"
 	}
+	r.Run(addr)
 }
