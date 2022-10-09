931b91f68b65196ecc649b2e63d925c39a7ec2f9
Drobyshev Alexander <drobyshev83@gmail.com>
2022-10-09 08:26:49 +0300

    fix

--- user/services/user.go
+++ user/services/user.go
@@ -18,21 +18,25 @@ type Server struct {
 }
 
 func (s *Server) GetUser(ctx context.Context, in *models.GetUserRequest) (*models.GetUserResponse, error) {
+	log.Println("")
+	log.Println("***** GetUser ******")
 	log.Println("Received:", in.Username)
 	if in.Username == "crash" {
 		rand.Seed(time.Now().UnixNano())
 		r := rand.Intn(10)
 		log.Println("Rand:", r)
 		if r == 1 {
+			log.Println("***** GetUser crashed ******")
 			log.Panicln("Exit")
 		}
 	} else {
 		s.V++
 		if s.V%2 == 0 {
-			log.Println("Sent Internal")
+			log.Println("***** GetUser status Internal error ******")
 			return nil, status.Error(codes.Internal, in.Username)
 		}
 	}
+	log.Println("***** GetUser status OK ******")
 	return &models.GetUserResponse{
 		Username: "Alex",
 		Password: "Pass",
